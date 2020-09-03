82491b5a6aa3a036e9a3b49657c2ae9887a4d4d6
alpha-martinez <martinezalphaa@gmail.com>
2020-09-03 13:00:20 -0400

    [passport] set up options and export passport function

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,5 +3,23 @@ require('dotenv').config();
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
+const { deserializeUser } = require('passport');
 // const User = mongoose.model('User');
 
+const options = {}
+options.jwtFromRequest = RxtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = process.env.JWT_SECRET;
+
+
+module.exports = (passport) => {
+    passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+        deserializeUser.findById(jwt_payload.id)
+        .then(user => {
+            if(user) {
+                return done(null, user);
+            }
+
+            return done(null, false)
+        })
+    }))
+}
